461a398d5383ddc368c5f4c2ed458bbffe3cadb5
vasu <vasuvaghasiya103@gmail.com>
2023-12-14 17:46:43 +0530

    update base condition

--- src/Fibonacci_series.java
+++ src/Fibonacci_series.java
@@ -5,8 +5,8 @@ public class Fibonacci_series {
     }
 
     private static int fibonacciSeries(int n) {
-        if(n==0) return 0;
-        if(n==1 || n==2) return 1;
+        if(n==1) return 0;
+        if(n==2) return 1;
         return fibonacciSeries(n-1) + fibonacciSeries(n-2);
 
     }
